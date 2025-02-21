eba2394f6a67f0b624e845354ddfcf06ba05fbd9
d0uglas77p <douglas_1313@hotmail.com>
2025-02-21 20:23:02 -0300

    Sessão de Autenticação do Administrador

---
+++ src/main/java/kickboxing/controler/MapearPaginasController.java
@@ -0,0 +1,59 @@
+package kickboxing.controler;
+
+import jakarta.servlet.http.HttpSession;
+import kickboxing.model.Admin;
+import kickboxing.service.AdminService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class MapearPaginasController {
+
+    private AdminService adminService;
+
+    @GetMapping("/index")
+    public String indexPage() {
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/recuperarSenha")
+    public String mostrarFormularioRecuperacao(@RequestParam("token") String token, Model model) {
+        if (adminService.isTokenValido(token)) {
+            model.addAttribute("token", token);  // PASSA O TOKEN PARA O THYMELEAF
+            return "recuperarSenha";
+
+        } else {
+            model.addAttribute("errorMessage", "Token de recuperação inválido ou expirado.");
+            return "redirect:/index";
+        }
+    }
+
+    @GetMapping("/administracao")
+    public String logado(HttpSession session, RedirectAttributes redirectAttributes, Model model) {
+        if (session.getAttribute("adminLogado") == null) {
+            redirectAttributes.addFlashAttribute("errorMessage", "Você precisa ser um administrador para acessar essa página.");
+            return "redirect:/index";
+        }
+
+        Admin admin = (Admin) session.getAttribute("adminLogado");
+
+        if (admin != null && admin.getDataRegistro() != null) {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            String dataFormatada = admin.getDataRegistro().format(formatter);
+            model.addAttribute("dataRegistroFormatada", dataFormatada);
+        }
+
+        model.addAttribute("admin", admin);
+        return "administracao";
+    }
+}
